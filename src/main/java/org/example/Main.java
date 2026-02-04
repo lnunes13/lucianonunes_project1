e43624a0284d47ebfef73a7ec4ffc54083c7c689
lnunes13 <lucianoaugusto13@gmail.com>
2026-02-04 09:41:26 -0500

    forgot to add files xD

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,64 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.FileInputStream;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import org.vosk.LogLevel;
+import org.vosk.Recognizer;
+import org.vosk.LibVosk;
+import org.vosk.Model;
+
+public class Main {
+    public static void main(String[] argv) throws IOException, UnsupportedAudioFileException {
+        AudioRecorder recorder = new AudioRecorder();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Press ENTER to start recording...");
+        scanner.nextLine();
+
+        // Thread for recording sound
+        Thread stopper = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    scanner.nextLine()
+;                   Thread.sleep(100);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                recorder.stopRecording();
+                System.out.println("Processing speech to text...");
+            }
+        });
+
+
+        stopper.start();
+        System.out.println("Recording started,");
+        System.out.println("Press ENTER to stop recording...");
+        recorder.startRecording();
+
+        LibVosk.setLogLevel(LogLevel.DEBUG);
+
+        try (Model model = new Model("vosk-model-en-us-0.22");
+             InputStream ais = AudioSystem.getAudioInputStream(new BufferedInputStream(new FileInputStream("recording_buffer.wav")));
+             Recognizer recognizer = new Recognizer(model, 16000)) {
+
+            int nbytes;
+            byte[] b = new byte[4096];
+            while ((nbytes = ais.read(b)) >= 0) {
+                if (recognizer.acceptWaveForm(b, nbytes)) {
+                    System.out.println(recognizer.getResult());
+                } else {
+                    System.out.println(recognizer.getPartialResult());
+                }
+            }
+
+            System.out.println(recognizer.getFinalResult());
         }
     }
 }
