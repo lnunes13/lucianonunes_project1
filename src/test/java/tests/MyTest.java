20d0757163c9715ae211f65427fb569d62ed621f
lnunes13 <lucianoaugusto13@gmail.com>
2026-02-05 18:29:16 -0500

    implemented fully working test

--- src/test/java/tests/MyTest.java
+++ src/test/java/tests/MyTest.java
@@ -7,6 +7,7 @@
 
 package tests;
 
+import org.apache.commons.text.similarity.LevenshteinDistance;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.vosk.LibVosk;
@@ -16,29 +17,40 @@ import org.vosk.Recognizer;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 public class MyTest {
+    public static final double maxLevenshteinDistanceTolerance = 30;
+
+    // Uses Levenshtein distance as a metric to compare the two strings.
     @Test
     public void testModel() throws IOException, UnsupportedAudioFileException {
+        // Build model
         LibVosk.setLogLevel(LogLevel.DEBUG);
-
         try (Model model = new Model("vosk-model-en-us-0.22");
              InputStream ais = AudioSystem.getAudioInputStream(
                      new BufferedInputStream(new FileInputStream("src/test/test_audio_sample.wav")));
              Recognizer recognizer = new Recognizer(model, 8000)) {
 
+            // Use model to process audio
             int nbytes;
             byte[] b = new byte[4096];
             while ((nbytes = ais.read(b)) >= 0) {
                 recognizer.acceptWaveForm(b, nbytes);
             }
 
+            // Read sample testing text from file and get transcribed text from AI model.
             JSONObject jsonObject = new JSONObject(recognizer.getFinalResult());
-            System.out.println(jsonObject.getString("text"));
+            BufferedReader reader = new BufferedReader(new FileReader("src/test/java/correct_test.txt"));
+            String correctSpeech = reader.readLine();
+            String transcribedSpeech = jsonObject.getString("text");
+
+            // Compute leverstein distance between both strings
+            LevenshteinDistance LevenshteinInstance = new LevenshteinDistance();
+            int distance = LevenshteinInstance.apply(correctSpeech, transcribedSpeech);
+
+            // Assert distance is below tolerance.
+            assert(distance <= maxLevenshteinDistanceTolerance);
         }
     }
 }
